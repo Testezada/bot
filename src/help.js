26f3513ea601e7265a3bcf5ca642ec19986e0ab8
testevrzn <78117358+testevrzn@users.noreply.github.com>
2021-02-11 21:11:08 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,175 @@
+const help = (prefix,pushname2, user, limitt) => {
+	return `	
+❉──────────❉
+
+   「 *VRAU BOT* 」
+
+❉──────────❉
+╔════════════════════
+║╭──❉ *INFORMAÇÕES DO USUARIO* ❉──
+║│➸NOME DO USUARIO : *EM BREVE*
+║│➸LIMITE DE COMANDOS : *EM BREVE*
+║╰───────────    
+╚════════════════════
+╔════════════════════
+║╭──❉ *INFORMAÇÕES* ❉──
+║│➸ Prefix: 「  ${prefix}  」
+║│➸ Criadores : *TIO VRAU* \ *TIO PAIN*
+║│➸ Numero do meu criador 1: https://wa.me/5511985024191
+║│➸ Numero do meu criador 2: https://wa.me/559885018147
+║╰───────────    
+╠════════════════════
+║╭──❉ *STICKERS* ❉──
+║│➸ ${prefix}sticker OU ${prefix}stickergif
+║│Sobre : O comando  ${prefix}sticker OU ${prefix}stickergif faz figurinha
+║╰───────────
+║╭───────────
+║│➸${prefix}toimg
+║│Sobre : O comando ${prefix}toimg e para converter stickers  em fotos 
+║╰───────────
+║╭───────────
+║│➸${prefix}tomp3
+║│Sobre : o comando ${prefix}tomp3 e para converter videos em audio
+║╰───────────
+╠════════════════════
+║╭──❉ *PREMIUM* ❉──
+║│➸${prefix}play
+║│Sobre : esse comando ${prefix}play e para abaixar musica
+║╰───────────
+║╭───────────
+║│➸${prefix}joox
+║│Sobre : esse comando ${prefix}joox e para abaixar musica/videos
+║╰───────────
+║╭───────────
+║│➸${prefix}ytmp3
+║│Sobre : esse comando ${prefix}ytmp3 e para abaixar musica
+║╰───────────
+║╭───────────
+║│➸${prefix}snack
+║│Sobre : esse comando ${prefix}snack e para abaixar videos de algum lugar ai
+║╰───────────
+║╭───────────
+║│➸${prefix}ytmp4
+║│Sobre : esse comando ${prefix}ytmp4 e para abaixar videos com a url
+║╰───────────
+║╭───────────
+║│*EM BREVE MAIS FUNÇOES PREMIUM*
+║│
+║╰───────────
+╠════════════════════
+║╭──❉ *AUDIO* ❉──
+║│➸${prefix}tts
+║│Sobre : esse comando e para o bot falar\n║│exemplo : ${prefix}tts pt vrau bot
+║╰───────────
+╠════════════════════
+║╭──❉ *FOTOS E OUTRAS COISINHAS* ❉──
+║│➸${prefix}loli
+║│Sobre : esse ai e pros lolicon kkkkk
+║╰───────────
+║╭───────────
+║│➸${prefix}shota
+║│Sobre : as vezes vai mais e uma foto de um menino
+║╰───────────
+║╭───────────
+║│➸${prefix}imagem
+║│Sobre : esse pesquisa qual quer foto do pinterest
+║╰───────────
+║╭───────────
+║│➸${prefix}neko
+║│Sobre : esse e pros lolicon que gosta\n║│da oni-chan com rabinho e orelha de gato
+║╰───────────
+╠════════════════════
+║╭──❉ *OUTROS* ❉──
+║│➸ ${prefix}meulink
+║│Sobre : mando o wa.me/Seu numero
+║╰───────────
+║╭───────────
+║│➸ ${prefix}travazap
+║│Sobre : so pelo nome vcs ja sabem kkk
+║╰───────────
+║╭───────────
+║│➸ ${prefix}afk
+║│Sobre : esse comando e pra quele amigo corno que n deixa vc quieto
+║╰───────────
+║╭───────────
+║│➸ ${prefix}tempo
+║│Sobre : esse negocio ai nem sei pra que serve ksksksksks
+║╰───────────
+╠════════════════════
+║╭──❉ *SOBRE* ❉──
+║│➸ ${prefix}info
+║│Sobre : isso e para saber as info do bot
+║╰───────────
+║╭───────────
+║│➸ ${prefix}blocklist
+║│Sobre : lista de bloqueados do bot
+║╰───────────
+║╭───────────
+║│➸  ${prefix}ping
+║│Sobre : o ping do bot
+║╰───────────
+╠════════════════════
+║╭──❉ *ESSE AGORA E SO PRO TIO VRAU* ❉──
+║│➸  ${prefix}prefixo
+║│Sobre : muda o prefixo
+║╰───────────
+║╭───────────
+║│➸ ${prefix}block
+║│Sobre : bloquea a pessoa
+║╰───────────
+║╭───────────
+║│➸ ${prefix}bc
+║│Sobre : brodcast esamerda da ban no bot usa n
+║╰───────────
+║╭───────────
+║│➸ ${prefix}bcgc
+║│Sobre : brodcast diferenciado esamerda da ban no bot usa n
+║╰───────────
+║╭───────────
+║│➸ ${prefix}clone
+║│Sobre : clona a foto do amigo
+║╰───────────
+║╭───────────
+║│➸ ${prefix}clearall
+║│Sobre : limpa tudo do chat do bot
+║╰───────────
+║╭───────────
+║│➸ ${prefix}listaprem
+║│Sobre : lista dos usuarios premios
+║╰───────────
+║╭───────────
+║│➸ ${prefix}addprem
+║│Sobre : add um usuario a premium
+║╰───────────
+║╭───────────
+║│➸ ${prefix}kickprem
+║│Sobre : remove um usuario premium
+║╰───────────
+║╭───────────
+║│➸ ${prefix}ban
+║│Sobre : bane uma pessoa a usar comando do bot
+║╰───────────
+║╭───────────
+║│➸ ${prefix}unban
+║│Sobre : desbane o usuario a usar o bot
+║╰───────────
+╠════════════════════
+║╭──❉ *OUTROS MENU* ❉──
+║│➸ ${prefix}menuadmin
+║╰───────────
+║╭───────────
+║│➸ ${prefix}toinmenu
+║╰───────────
+║╭───────────
+║│➸ ${prefix}nsfwmenu
+║╰───────────
+║╭───────────
+║│➸ ${prefix}idiomas
+║╰───────────
+╠════════════════════
+║ TITIO VRAU O BRABO
+╚════════════════════
+`
+}
+
+exports.help = help
