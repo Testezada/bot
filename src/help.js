38ef58b473cc0d0456a4d98db2d356369a431e30
testevrzn <78117358+testevrzn@users.noreply.github.com>
2021-02-13 11:31:07 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -14,7 +14,7 @@ const help = (prefix,pushname2, user, limitt) => {
 ╔════════════════════
 │╭──◓ *INFORMAÇÕES* ◓──
 ║│➸ Prefix: 「  ${prefix}  」
-║│➸ Criadores : *Papai Vrau e Mamãe Dreza*
+║│➸ Criadores : *Papai Vrau*
 ║│➸ Numero do meu criador : https://wa.me/559885018147
 ║╰───────────    
 ╠════════════════════
